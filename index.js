28d274662c6619694badd8f77c73257ba2f38d9a
Shahzaib Shafiq <73356944+shahzaib-shafiq@users.noreply.github.com>
2024-10-01 20:42:32 +0500

    Dynamic Routing in Express

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
-const express = require('express')
+const express = require('express');
 const ejs = require('ejs');
-const app = express()
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+const app = express();
+const path=require('path')
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname,'public')))
 
 
 app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+  res.render('index'); 
+});
 
-app.listen(3000)
+
+app.get('/profile/:username', function (req, res) {
+  res.send(`welcome ${req.params.username}`); 
+  
+});
+
+
+app.get('/profile/:username/:age', function (req, res) {
+  //res.send(`welcome ${req.params.username} and Age ${req.params.age}`);
+  res.send(req.params);
+   
+  
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
